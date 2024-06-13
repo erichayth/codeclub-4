3dc03e89f0ac96f9c58aba674f769be948f90c84
Eric Hayth <ehayth@cloudflare.com>
2024-06-13 09:51:56 -0400

    code club round 5

--- src/app/api/profile/route.ts
+++ src/app/api/profile/route.ts
@@ -17,23 +17,31 @@ export async function POST(request: NextRequest) {
 
 async function handler(request: NextRequest) {
   try {
+    console.log('Received request:', request);
+
     // Get the 'UserID' from the request headers
     const userId = request.headers.get('userid');
+    console.log('UserID:', userId);
 
     if (!userId) {
+      console.log('UserID header is missing');
       return new NextResponse('UserID header is missing', { status: 400 });
     }
 
     // Fetch the value from the KV store
     const authToken = await codeclub_namespace.get(userId);
+    console.log('Auth token:', authToken);
 
     if (!authToken) {
+      console.log('Auth token not found for UserID');
       return new NextResponse('Auth token not found for UserID', { status: 404 });
     }
 
     // Create a new headers object with the 'Auth-Token' header
     const modifiedHeaders: HeadersInit = new Headers(request.headers);
     modifiedHeaders.set('Auth-Token', authToken);
+
+    console.log('Modified headers:', modifiedHeaders);
 
     // Create the fetch options
     const fetchOptions: RequestInit = {
@@ -43,14 +51,21 @@ async function handler(request: NextRequest) {
 
     if (request.method !== 'GET' && request.method !== 'HEAD') {
       fetchOptions.body = await request.text();
+      console.log('Request body:', fetchOptions.body);
     }
+
+    console.log('Fetch options:', fetchOptions);
 
     // Forward the request to the specified URL
     const response = await fetch('https://orange.ent.haythnet.com/get', fetchOptions);
+    console.log('Fetch response status:', response.status);
 
     // Send back the response from the forwarded request
     const responseBody = await response.text();
+    console.log('Fetch response body:', responseBody);
+
     return new NextResponse(responseBody, { status: response.status });
+
   } catch (error) {
     console.error('Error in handler:', error);
     return new NextResponse('Internal Server Error', { status: 500 });
