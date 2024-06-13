f90ea70885598f15740b112ec3bdafdea34cb348
Eric Hayth <ehayth@cloudflare.com>
2024-06-13 13:13:23 -0400

    code club round 5

--- src/app/api/profile/route.ts
+++ src/app/api/profile/route.ts
@@ -1,3 +1,5 @@
+import { NextRequest, NextResponse } from 'next/server';
+
 // Define the runtime environment
 export const runtime = 'edge';
 
@@ -5,28 +7,28 @@ export interface Env {
     CODECLUB_NAMESPACE: KVNamespace;
 }
 
-export async function GET(request: Request, env: Env): Promise<Response> {
+export async function GET(request: NextRequest, env: Env): Promise<NextResponse> {
   try {
     const userID = request.headers.get("UserID");
 
     if (!userID) {
-      return new Response("UserID is missing", { status: 400 });
+      return new NextResponse("UserID is missing", { status: 400 });
     }
 
     const value = await env.CODECLUB_NAMESPACE.get(userID);
 
     if (!value) {
-      return new Response("UserID is not valid", { status: 403});
+      return new NextResponse("UserID is not valid", { status: 403});
     }
 
     else {
-    let newRequest = new Request(request);
+    let newRequest = new NextRequest(request);
     newRequest.headers.set("Auth-Token", value);
     let authValue = newRequest.headers.get("Auth-Token");
-    return new Response('User token for UserID added to Auth-Token header', { status: 200});
+    return new NextResponse('User token for UserID added to Auth-Token header', { status: 200});
     }
 
   } catch (err) {
-    return new Response('Internal Server Error', { status: 500 });
+    return new NextResponse('Internal Server Error', { status: 500 });
   }
 }
